bce4f5d715c74d65ba5bc4460850333c741d4412
Antonius Riki Hermawan <rikihermawan9342@gmail.com>
2024-07-20 10:29:17 +0700

    UAS Pemrograman Berbasis Objek

---
+++ UasPboNoteApp/src/main/java/uas/noteapp/DataStorage.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package uas.noteapp;
+
+import java.util.List;
+
+/**
+ *
+ * @author DKID
+ */
+public interface DataStorage {
+    void writeData(String note);
+
+    List<String> readData();
+
+    void deleteData(String note);
+}
